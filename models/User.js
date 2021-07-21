438e7e6d1ea11801f4fd4645e83923d1f4095638
Valdecir Luciano <valdecirlucianoti@gmail.com>
2021-07-21 16:57:08 -0300

    Subundo p projeto para o github

---
+++ models/User.js
@@ -0,0 +1,12 @@
+class User{
+    constructor(name, gender, birth, county, email, password, photo, admin){
+        this.name = name;
+        this.gender = gender;
+        this.birth = birth;
+        this.county = county;
+        this.email = email;
+        this.password = password;
+        this.photo = photo;
+        this.admin = admin;
+    }
+}
